9e967a418fa9b4fe4d2e3a347e6c668c820fd6a2
samwebsite876 <samwebsite876@users.noreply.github.com>
2026-05-16 15:19:36 -0500

    additions

--- api.py
+++ api.py
@@ -8,7 +8,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Jamaica.876",
+        password="",
         database="university",
         auth_plugin='mysql_native_password'
     )
